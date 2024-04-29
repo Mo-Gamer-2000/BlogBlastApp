252f7770bf04aac8de54e184d533f0c3c4e70a7e
Moeez Abdul <moeezrajput2000@gmail.com>
2024-04-29 13:46:36 +0100

    Added Event Listener for the navbar in functions.js

--- BlogBlast/wwwroot/assets/js/functions.js
+++ BlogBlast/wwwroot/assets/js/functions.js
@@ -1,8 +1,19 @@
-/*jQuery( document ).ready(function() {
-
-    $(window).scroll(function(){
-    $('.topnav').toggleClass('scrollednav py-0', $(this).scrollTop() > 50);
-    });
-    
+// Add Event Lister 
+document.addEventListener("DOMContentLoaded", _ => {
+    // Create a Const to get the "topnav" class element by id from index[0] from the list of pages - for NavBar
+    const topNav = document.getElementsByClassName("topnav")[0];
+    // Do a simple if check statement to see if topNav is available ot not
+    if (topNav) {
+        window.scroll = () => {
+            // Also, check if the scroll is greater then 50 pixels
+            if (window.scrollY > 50) {
+                // If so, add these classes - scrollednav py-0
+                topNav.classList.add('scrollednav', 'py-0')
+            }
+            else {
+                // Else, remove those classes - scrollednav py-0
+                topNav.classList.remove('scrollednav', 'py-0')
+            }
+        };
+    }
 });
-*/
