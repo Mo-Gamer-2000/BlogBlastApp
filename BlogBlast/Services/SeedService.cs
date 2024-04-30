ca25bb5ec3a671d4f9169a872e271127c83eed3e
Moeez Abdul <moeezrajput2000@gmail.com>
2024-04-30 14:56:34 +0100

    Implemented Seed Admin Role, with Error Handling!

--- BlogBlast/Services/SeedService.cs
+++ BlogBlast/Services/SeedService.cs
@@ -1,9 +1,16 @@
 ﻿using BlogBlast.Data;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 
 namespace BlogBlast.Services;
 
-
+internal static class AdminAccount
+{
+	public const string Name = "Moeez Abdul";
+	public const string Email = "Moeez@gmail.com";
+	public const string Role = "Admin";
+	public const string Password = "Admin24680";
+}
 public class SeedService
 {
 	private readonly ApplicationDbContext _context;
@@ -21,11 +28,25 @@ public class SeedService
 
     public async Task SeedDataAsync()
 	{
-		// Seed Admin Role
+        // Seed Admin Role
+		// Check if this role (Admin) exists in our DB
+        if (await _userManager.FindByNameAsync(AdminAccount.Role) is null)
+        {
+            // This Admin role does not exist in the DB
+			// Then, create one
+			var adminRole = new IdentityRole(AdminAccount.Role);
 
+			var result = await _roleManager.CreateAsync(adminRole);
+			// Check if Admin was not created
+            if (!result.Succeeded)
+            {
+				var errorMessage = result.Errors.Select(e => e.Description);
+				throw new Exception(string.Join(Environment.NewLine, errorMessage));
+            }
+        }
 
-		// Seed Admin User
+        // Seed Admin User
 
-		// Seed Initial Categories
-	}
+        // Seed Initial Categories
+    }
 }
