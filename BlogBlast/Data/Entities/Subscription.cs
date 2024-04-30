54a6745580605a9d5fea8295687b997f3fb9e4d5
Moeez Abdul <moeezrajput2000@gmail.com>
2024-04-30 11:48:52 +0100

    Created Subscription Entity

---
+++ BlogBlast/Data/Entities/Subscription.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogBlast.Data.Entities;
+public class Subscription
+{
+    public long Id { get; set; }
+    [EmailAddress, Required, MaxLength(150)]
+    public string Email { get; set; }
+    [Required, MaxLength(25)]
+    public string Name { get; set; }
+    public DateTime SubscribedOn { get; set; }
+}
