632229566475b7c69fded735de60930cb9840264
Moeez Abdul <moeezrajput2000@gmail.com>
2024-04-30 10:36:57 +0100

    Created Featured Blog Post as a Resuable Component, and rendered it on HomePage, also Imported path to _imports for ease!

--- BlogBlast/wwwroot/assets/js/functions.js
+++ BlogBlast/wwwroot/assets/js/functions.js
@@ -1,12 +1,12 @@
 // TESTING...
-alert('functions.js');
+// alert('functions.js');
 // Result = it is loading
 
 // Add Event Lister 
 document.addEventListener("DOMContentLoaded", _ => {
     // Create a Const to get the "topnav" class element by id from index[0] from the list of pages - for NavBar
     const topNav = document.getElementsByClassName("topnav")[0];
-    // Do a simple if check statement to see if topNav is available ot not
+    // Do a simple if check statement to see if topNav is available or not
     if (topNav) {
         window.onscroll = () => {
             // Also, check if the scroll is greater then 50 pixels
