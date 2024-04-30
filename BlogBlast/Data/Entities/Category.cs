447095cd9f6a111bf195bcd5f2237568b272c95d
Moeez Abdul <moeezrajput2000@gmail.com>
2024-04-30 11:35:47 +0100

    Created Entity Directory and created Category Entity

---
+++ BlogBlast/Data/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogBlast.Data.Entities;
+public class Category
+{
+    public short Id { get; set; }
+    [Required, MaxLength(50)]
+    public string Name { get; set; }
+    public string Slug { get; set; }
+    public bool VisibleOnNavbar { get; set; }
+}
