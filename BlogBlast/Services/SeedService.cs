7467107ce2eb1ac5ca6c87bc096524d47bf9e7ea
Moeez Abdul <moeezrajput2000@gmail.com>
2024-04-30 18:00:11 +0100

    Added Admin Account Constants in the appsettings.json file, and Injecting them into the Seed Service, and accessing the values from there.
    Also Fixed null reference warnings and added null-forgiving operator ! to remove them!
    
    Tested and Working!

--- BlogBlast/Services/SeedService.cs
+++ BlogBlast/Services/SeedService.cs
@@ -2,16 +2,17 @@
 using BlogBlast.Data.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace BlogBlast.Services;
 
-internal static class AdminAccount
+/*internal static class AdminAccount
 {
 	public const string Name = "Moeez Abdul";
 	public const string Email = "Moeez@gmail.com";
 	public const string Role = "Admin";
 	public const string Password = "Admin24680!";
-}
+}*/
 
 public interface ISeedService
 {
@@ -24,13 +25,15 @@ public class SeedService : ISeedService
 	private readonly IUserStore<ApplicationUser> _userStore;
 	private readonly UserManager<ApplicationUser> _userManager;
 	private readonly RoleManager<IdentityRole> _roleManager;
+	private readonly IConfiguration _configuration;
 
-	public SeedService(ApplicationDbContext context, IUserStore<ApplicationUser> userStore, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+	public SeedService(ApplicationDbContext context, IUserStore<ApplicationUser> userStore, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
 	{
 		_context = context;
 		_userStore = userStore;
 		_userManager = userManager;
 		_roleManager = roleManager;
+		_configuration = configuration;
 	}
 
 	// Created method to Automatically Migrate the DB - WARNING only for Development Mode - **REMOVE THIS BEFORE PRODUCTION**
@@ -47,18 +50,24 @@ public class SeedService : ISeedService
 
 	public async Task SeedDataAsync()
 	{
+		// Access admin account constants from IConfiguration
+		string? adminName = _configuration["AdminAccount:Name"];
+		string? adminEmail = _configuration["AdminAccount:Email"];
+		string? adminRole = _configuration["AdminAccount:Role"];
+		string? adminPassword = _configuration["AdminAccount:Password"];
+
 		await MigrateDatabaseAsync(); // Called the Method ^^^
 
 		// Seed Admin Role
 		// Check if this role (Admin) exists in our DB
-		if (await _roleManager.FindByNameAsync(AdminAccount.Role) is null) // Typo Error fixed on this line, before _userRole, changed to _roleManager, SOLVED IT!
+		if (await _roleManager.FindByNameAsync(adminRole!) is null)
 		{
 			// Check Admin role does not exist in the DB
 			// Then, create one
-			var adminRole = new IdentityRole(AdminAccount.Role);
+			var role = new IdentityRole(adminRole!);
 
-			var result = await _roleManager.CreateAsync(adminRole);
-			// Check if Admin was not created
+			var result = await _roleManager.CreateAsync(role);
+			// Check if Admin role was not created
 			if (!result.Succeeded)
 			{
 				var errorMessage = result.Errors.Select(e => e.Description);
@@ -67,18 +76,17 @@ public class SeedService : ISeedService
 		}
 
 		// Seed Admin User
-
-		var adminUser = await _userManager.FindByEmailAsync(AdminAccount.Email);
+		var adminUser = await _userManager.FindByEmailAsync(adminEmail!);
 		if (adminUser is null)
 		{
 			// Check Admin user does not exist in the DB
 			// Then, create one
 			adminUser = new ApplicationUser();
-			adminUser.Name = AdminAccount.Name;
-			await _userStore.SetUserNameAsync(adminUser, AdminAccount.Email, CancellationToken.None);
+			adminUser.Name = adminName!;
+			await _userStore.SetUserNameAsync(adminUser, adminEmail, CancellationToken.None);
 			var emailStore = (IUserEmailStore<ApplicationUser>)_userStore;
-			await emailStore.SetEmailAsync(adminUser, AdminAccount.Email, CancellationToken.None);
-			var result = await _userManager.CreateAsync(adminUser, AdminAccount.Password);
+			await emailStore.SetEmailAsync(adminUser, adminEmail, CancellationToken.None);
+			var result = await _userManager.CreateAsync(adminUser, adminPassword!);
 
 			// Check if Admin was not created
 			if (!result.Succeeded)
@@ -89,13 +97,14 @@ public class SeedService : ISeedService
 		}
 
 		// Seed Initial Categories
-		// Check if there is not category
+		// Check if there are no categories
 		if (!await _context.Categories.AsNoTracking().AnyAsync())
 		{
-			// Check if there is no categories in the DB
-			// Then, create category
+			// Check if there are no categories in the DB
+			// Then, create categories
 			await _context.Categories.AddRangeAsync(Category.GetSeedCategories());
 			await _context.SaveChangesAsync();
 		}
 	}
+
 }
