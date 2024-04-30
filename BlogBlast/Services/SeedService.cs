c9d782a475f73d1224f9f0eadc83257f6352ec8c
Moeez Abdul <moeezrajput2000@gmail.com>
2024-04-30 14:31:51 +0100

    Created Dependencies and a void method to seedData Async

---
+++ BlogBlast/Services/SeedService.cs
@@ -0,0 +1,31 @@
+﻿using BlogBlast.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlogBlast.Services;
+
+
+public class SeedService
+{
+	private readonly ApplicationDbContext _context;
+	private readonly IUserStore<ApplicationUser> _userStore;
+	private readonly UserManager<ApplicationUser> _userManager;
+	private readonly RoleManager<IdentityRole> _roleManager;
+
+	public SeedService(ApplicationDbContext context,IUserStore<ApplicationUser> userStore, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+    {
+		_context = context;
+		_userStore = userStore;
+		_userManager = userManager;
+		_roleManager = roleManager;
+	}
+
+    public async Task SeedDataAsync()
+	{
+		// Seed Admin Role
+
+
+		// Seed Admin User
+
+		// Seed Initial Categories
+	}
+}
