feb67cbce059a14de7b406c3a17e5f27ce1f7fc7
Moeez Abdul <moeezrajput2000@gmail.com>
2024-05-01 11:46:56 +0100

    Created Service to Category Creation, inProgress..

---
+++ BlogBlast/Services/CategoryService.cs
@@ -0,0 +1,22 @@
+﻿using BlogBlast.Data;
+using BlogBlast.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogBlast.Services;
+public class CategoryService
+{
+    private readonly IDbContextFactory<ApplicationDbContext> _contextFactory;
+
+    public CategoryService(IDbContextFactory<ApplicationDbContext> contextFactory)
+    {
+        _contextFactory = contextFactory;
+    }
+
+    public async Task<Category[]> GetCategoriesAsync()
+    {
+        // Dispose after code execution leaves the body of this method
+        using var context = _contextFactory.CreateDbContext();
+        var categories = await context.Categories.AsNoTracking().ToArrayAsync();
+        return categories;
+    }
+}
