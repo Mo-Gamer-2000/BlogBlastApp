bbe5cf363aafee38bdd44c66212b2665b20cde68
Moeez Abdul <moeezrajput2000@gmail.com>
2024-05-01 12:38:29 +0100

    Implemented Get all Categories and Save Category Method.
    Created a template method to create a DBcontext.
    
    AND created a static method in the extensions to modify the slug using Regex.

--- BlogBlast/Data/Entities/Category.cs
+++ BlogBlast/Data/Entities/Category.cs
@@ -5,9 +5,9 @@ public class Category
 {
     public short Id { get; set; }
     [Required, MaxLength(50)]
-    public string? Name { get; set; }
+    public string Name { get; set; }
     [Required, MaxLength(75)]
-    public string? Slug { get; set; }
+    public string Slug { get; set; }
     public bool VisibleOnNavbar { get; set; }
 
 	// Created an Array for Seed Categories
--- BlogBlast/Services/CategoryService.cs
+++ BlogBlast/Services/CategoryService.cs
@@ -12,11 +12,65 @@ public class CategoryService
         _contextFactory = contextFactory;
     }
 
+    // Create template method
+    private async Task<TResult> ExecuteOnContext<TResult>(Func<ApplicationDbContext, Task<TResult>> query)
+    {
+        // Create a DB  context
+        using var context = _contextFactory.CreateDbContext();
+        // Return the result and dispose it
+        return await query.Invoke(context);
+    }
+
+    // Get All Categories Method
     public async Task<Category[]> GetCategoriesAsync()
     {
-        // Dispose after code execution leaves the body of this method
-        using var context = _contextFactory.CreateDbContext();
-        var categories = await context.Categories.AsNoTracking().ToArrayAsync();
-        return categories;
+        return await ExecuteOnContext(async context =>
+        {
+            // Dispose after code execution leaves the body of this method - Passsed to inner lapda function, then it will execute
+            var categories = await context.Categories.AsNoTracking().ToArrayAsync();
+            return categories;
+        });
+    }
+
+    // Save Category Method
+    public async Task<Category> SaveCategoryAsync(Category category)
+    {
+        return await ExecuteOnContext(async context =>
+        {
+            // Check for category exsitance
+            if (category.Id == 0)
+            {
+                // If the category = 0, which means it is a new category
+                //  Check if category exists in the DB
+                if (await context.Categories.AsNoTracking().AnyAsync(c=> c.Name == category.Name))
+                {
+                    // If the category exists with that name, then throw an error
+                    throw new InvalidOperationException($"Category with the name provided: {category.Name} already exists!");
+                }
+                // If the category does not exist, then we create it and save it
+                // Create a slug for category
+                category.Slug = category.Name.ToSlug();
+                await context.Categories.AddAsync(category);
+                await context.SaveChangesAsync();
+            }
+            else
+            {
+                // Else,  //  Check if category exists in the DB
+                if (await context.Categories.AsNoTracking().AnyAsync(c => c.Name == category.Name && c.Id != category.Id)) // Check the name with different ID, Not the same!
+                {
+                    // If the category exists with that name and different ID, then throw an error
+                    throw new InvalidOperationException($"Category with the name provided: {category.Name} already exists!");
+                }
+                // Get category from DB, via id
+                var dbCategory = await context.Categories.FindAsync(category.Id);
+
+                // Check if the the names are equal
+                dbCategory.Name = category.Name;
+                dbCategory.VisibleOnNavbar = category.VisibleOnNavbar; // Then make it visible on the navbar
+                category.Slug = dbCategory.Slug; // Checking if the slug are equal, if not revert back to db slug
+            }
+            await context.SaveChangesAsync(); // Save the chnages
+            return category; // and return the category
+        });
     }
 }
