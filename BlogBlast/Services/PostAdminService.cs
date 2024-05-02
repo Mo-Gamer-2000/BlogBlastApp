47d53dd249c608359cf2fe52fafa6e180be0eb18
Moeez Abdul <moeezrajput2000@gmail.com>
2024-05-02 17:24:45 +0100

    Created Interface with Get blog post / post by id and save blog posts

--- BlogBlast/Services/PostAdminService.cs
+++ BlogBlast/Services/PostAdminService.cs
@@ -1,4 +1,15 @@
-﻿namespace BlogBlast.Services;
+﻿using BlogBlast.Components.Pages;
+using BlogBlast.Data.Entities;
+
+namespace BlogBlast.Services;
+
+public interface IPostAdminService
+{
+    Task<Post[]> GetBlogPostsAsync();
+    Task<Post[]> GetBlogPostsByIdAsync(int id); 
+    Task<Post[]> SaveBlogPostAsync(Post blogPost); 
+}
+
 public class PostAdminService
 {
     // Code Goes here
