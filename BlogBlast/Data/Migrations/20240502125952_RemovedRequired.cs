b6154a6019be2dcfd9f525ba0aa579d815b2062f
Moeez Abdul <moeezrajput2000@gmail.com>
2024-05-02 14:04:49 +0100

    UPDATED DB - New Migrations - REMOVED REQUIRED from SLUG in DATA ANNOTATIONS!

---
+++ BlogBlast/Data/Migrations/20240502125952_RemovedRequired.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogBlast.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedRequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
