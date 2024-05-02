c23999bad211f4dae6d3a0a4a46266225ef4221e
Moeez Abdul <moeezrajput2000@gmail.com>
2024-05-02 18:39:37 +0100

    Implemented slug generation and blog post saving functionality:
    - Added GenerateSlugAsync for unique slug creation based on post title.
    - Enhanced SaveBlogPostAsync to use the generated slug.
    - Implemented duplicate title checks, throwing exceptions when necessary.
    - Updated publication date logic.

--- BlogBlast/Services/PostAdminService.cs
+++ BlogBlast/Services/PostAdminService.cs
@@ -1,9 +1,7 @@
-﻿using BlogBlast.Components.Pages;
-using BlogBlast.Data;
+﻿using BlogBlast.Data;
 using BlogBlast.Data.Entities;
 using BlogBlast.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design.Internal;
 
 namespace BlogBlast.Services;
 
@@ -17,7 +15,7 @@ public interface IPostAdminService
     Task<Post?> GetBlogPostsByIdAsync(int id);
 
     // Method to save a blog post
-    Task<Post> SaveBlogPostAsync(Post blogPost);
+    Task<Post> SaveBlogPostAsync(Post blogPost, string userId);
 }
 
 // Implementation of the blog post admin service interface
@@ -69,9 +67,91 @@ public class PostAdminService : IPostAdminService
                 .FirstOrDefaultAsync(b => b.Id == id)
         );
 
-    // Method to save a blog post (not implemented)
-    public Task<Post> SaveBlogPostAsync(Post blogPost)
+    // Method to Generate a unique slug for a blog post based on its title
+    private async Task<string> GenerateSlugAsync(Post blogPost)
     {
-        throw new NotImplementedException();
+        // Generate a unique slug for the blog post
+        return await ExecuteOnContext(async context =>
+        {
+            string originalSlug = blogPost.Title.ToSlug();
+            string slug = originalSlug;
+            int count = 1;
+            // Check if the generated slug already exists, if yes, append a count to it
+            while (await context.BlogPosts.AsNoTracking().AnyAsync(b => b.Slug == slug))
+            {
+                slug = $"{originalSlug}-{count++}";
+            }
+            return slug;
+        });
+    }
+
+    // Method to save a blog post
+    public Task<Post> SaveBlogPostAsync(Post blogPost, string userId)
+    {
+        // Save a blog post to the database
+        return ExecuteOnContext(async context =>
+        {
+            if (blogPost.Id == 0)
+            {
+                // New Blog Post
+                var isDuplicateTitle = await context.BlogPosts
+                                        .AsNoTracking()
+                                        .AnyAsync(b => b.Title == blogPost.Title);
+                if (isDuplicateTitle)
+                {
+                    throw new InvalidOperationException("Blog Post with this Name Already Exists!");
+                }
+
+                // Generate and set slug
+                blogPost.Slug = await GenerateSlugAsync(blogPost);
+                blogPost.CreatedAt = DateTime.UtcNow;
+                blogPost.UserId = userId;
+
+                // Set published date if post is published
+                if (blogPost.IsPublished)
+                {
+                    blogPost.PublishedAt = DateTime.UtcNow;
+                }
+                await context.BlogPosts.AddAsync(blogPost);
+            }
+            else
+            {
+                // Existing Blog Post
+                var isDuplicateTitle = await context.BlogPosts
+                                        .AsNoTracking()
+                                        .AnyAsync(b => b.Title == blogPost.Title && b.Id != blogPost.Id);
+                if (isDuplicateTitle)
+                {
+                    throw new InvalidOperationException("Blog Post with this Title Already Exists!");
+                }
+
+                var dbBlog = await context.BlogPosts.FindAsync(blogPost.Id);
+
+                // Update blog post properties
+                dbBlog.Title = blogPost.Title;
+                dbBlog.Image = blogPost.Image;
+                dbBlog.Introduction = blogPost.Introduction;
+                dbBlog.Content = blogPost.Content;
+                dbBlog.CategoryId = blogPost.CategoryId;
+                dbBlog.IsPublished = blogPost.IsPublished;
+                dbBlog.IsFeatured = blogPost.IsFeatured;
+
+                // Update published date if post is published
+                if (blogPost.IsPublished)
+                {
+                    if (!dbBlog.IsPublished)
+                    {
+                        blogPost.PublishedAt = DateTime.UtcNow;
+                    }
+                }
+                else
+                {
+                    blogPost.PublishedAt = null;
+                }
+            }
+
+            await context.SaveChangesAsync();
+            return blogPost;
+        });
     }
 }
