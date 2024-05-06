72ecd8f7d04682524484f9586214a124c6bc8009
Moeez Abdul <moeezrajput2000@gmail.com>
2024-05-06 11:18:32 +0100

    Major Bug Fixed, where a Data Annotations, prevented the blog post to be created, as it created a dupplicate Category ID, due to Data type being Int, so changed to short as initially set  to short, which was conflicting when on conversion. And set then Category ID to start from 1, instead of 0.
    Fixed that and Migrated changes and Updated DB.
    
    After that, I created the UI for Manage Posts page, still under work!
    
    Also, created functionality to save Images functionality, major Code implementation.
    
    Then faced a bug where, after saving, the post, ws not showing, so I added Navigation manager to nevigate to the post and set auto refresh to true.

--- BlogBlast/Data/Entities/Post.cs
+++ BlogBlast/Data/Entities/Post.cs
@@ -8,24 +8,34 @@ public class Post
 
 	[Required, MaxLength(100)]
 	public string Title { get; set; }
+
     [MaxLength(125)]
     public string Slug { get; set; }
 
-	[Required, MaxLength(100)]
+	[MaxLength(100)]
 	public string Image { get; set; }
 
 	[Required, MaxLength(500)]
 	public string Introduction { get; set; }
-	[Required]
+
 	public string Content { get; set; }
-	[Range(1, int.MaxValue, ErrorMessage = "The Category field is required.")] // Setting CategoryID to start prom 1 to prevent having a category with ID 0, which will be in null in my case
-    public int CategoryId { get; set; }
+
+	[Range(1, short.MaxValue, ErrorMessage = "The Category field is required.")] // Setting CategoryID to start prom 1 to prevent having a category with ID 0, which will be in null in my case
+    public short CategoryId { get; set; }
+
     public string UserId { get; set; }
+
 	public int ViewCount { get; set; }
+
 	public bool IsPublished { get; set; }
+
     public bool IsFeatured { get; set; }
+
     public DateTime CreatedAt { get; set; }
+
     public DateTime? PublishedAt { get; set; }
+
 	public virtual Category Category { get; set;}
+
     public virtual ApplicationUser User { get; set; }
 }
