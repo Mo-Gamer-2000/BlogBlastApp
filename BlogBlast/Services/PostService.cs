85c9fbf892a0d7983c8e33809f1fa35d486b4a34
Moeez Abdul <moeezrajput2000@gmail.com>
2024-05-07 11:00:38 +0100

    Implementation of GetLatestPostsAsync method Completed

--- BlogBlast/Services/PostService.cs
+++ BlogBlast/Services/PostService.cs
@@ -103,7 +103,27 @@ namespace BlogBlast.Services
         /// <returns>An array of latest posts.</returns>
         public async Task<Post[]> GetLatestPostsAsync(int count, int categoryId = 0)
         {
+            // Executes the query asynchronously within the context and returns the result
+            return await QueryOnContextAsync(async context =>
+            {
+                // Construct the initial query to retrieve featured posts
+                var query = context.BlogPosts
+                                   .AsNoTracking()
+                                   .Include(p => p.Category) // Include related category information
+                                   .Include(b => b.User)     // Include related user information
+                                   .Where(b => b.IsPublished); // Filter by published status
 
+                // If a categoryId is specified, filter the query by that category
+                if (categoryId > 0)
+                {
+                    query = query.Where(b => b.CategoryId == categoryId);
+                }
+
+                // Order the results by publisehd date and take the specified number of posts
+                return await query.OrderByDescending(b => b.PublishedAt)
+                                  .Take(count)
+                                  .ToArrayAsync();
+            });
         }
 
         /// <summary>
